c206adec9ef8cf41f02dfadf51119b5189a2810a
Rsm080 <1877879201@qq.com>
2026-09-22 10:59:03 +0800

    Delete build directory

--- build/robot_gazebo_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jsm/robot_gazebo_sim/src/robot_gazebo_sim
-# Build directory: /home/jsm/robot_gazebo_sim/build/robot_gazebo_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/robot_gazebo_sim/colcon_command_prefix_test.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jsm/robot_gazebo_sim/install/robot_gazebo_sim/share/robot_gazebo_sim/package.sh"
