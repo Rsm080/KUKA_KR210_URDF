b1ca53b0712c608b90219d0a82505bd0ef7d310a
Rsm080 <1877879201@qq.com>
2026-09-22 10:56:26 +0800

    Add files via upload

---
+++ build/robot_gazebo_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jsm/robot_gazebo_sim/src/robot_gazebo_sim
+# Build directory: /home/jsm/robot_gazebo_sim/build/robot_gazebo_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/robot_gazebo_sim/colcon_command_prefix_test.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/jsm/robot_gazebo_sim/install/robot_gazebo_sim/share/robot_gazebo_sim/package.sh"
